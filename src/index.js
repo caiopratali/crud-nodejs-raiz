5a1a06ee38dffc730231134fa32be345990e05fd
Caio Pratali <caio@pratali.com.br>
2022-02-17 09:12:09 -0300

    controllers / routes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 const http = require('http');
-const users = require('./mocks/users.js');
+
+const UserController = require('./controllers/UserController');
+const routes = require('./routes');
 
 const server = http.createServer((request, response) => {
   console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
 
-  if(request.url === '/users' && request.method === 'GET') {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify(users));
+  const route = routes.find(routeObj => (routeObj.endpoint === request.url && routeObj.method === request.method));
+
+  if(route) {
+    route.handler(request, response);
   } else {
     response.writeHead(404, { 'Content-Type': 'application/json' });
     response.end(JSON.stringify({ error: 'not found' }));
   }
 });
 
-server.listen(3000, () => console.log('Server ON!'));
+server.listen(3000, () => console.log('🚀 Server ON! 🔗 URL: http://localhost:3000'));
